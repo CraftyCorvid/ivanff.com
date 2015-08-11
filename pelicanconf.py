64723c09a0fb4c97f913b6326de9bd9569f35375
Ivan <ivan@plugable.com>
2015-08-11 20:19:00 -0300

    Added bootstrap theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,11 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Ivan FF'
 SITENAME = u'Ivan FF'
-SITEURL = ''
+SITEURL = 'http://127.0.0.1:8080'
+
+# URLS
+TAGS_URL = 'tags'
+CATEGORIES_URL = 'categories'
 
 PATH = 'content'
 
@@ -30,6 +34,19 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 5
+DEFAULT_DATE = 'fs'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Theme
+THEME = "themes/bootstrap3"
+BOOTSTRAP_THEME = 'paper'
+DISPLAY_BREADCRUMBS = True
+DISPLAY_CATEGORY_IN_BREADCRUMBS = True
+DISPLAY_ARTICLE_INFO_ON_INDEX = True
+SHOW_ARTICLE_AUTHOR = True
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
+DISPLAY_CATEGORIES_ON_SIDEBAR = True
